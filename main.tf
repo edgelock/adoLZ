af611f8d8b703b5b162f49499bc74ebc128cc42a
Charles <charles@Charless-MacBook-Pro.local>
2024-04-18 09:07:06 -0400

    breaking up tf stuff

--- main.tf
+++ main.tf
@@ -13,29 +13,7 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-}
 
-resource "azurerm_resource_group" "vnet_rg" {
-  name     = var.resourcegroup_name
-  location = var.location
-  tags     = var.tags
-}
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
-  address_space       = var.vnet_address_space
-  location            = azurerm_resource_group.vnet_rg.location
-  resource_group_name = azurerm_resource_group.vnet_rg.name
-  tags                = var.tags
-}
 
-resource "azurerm_subnet" "subnet" {
-  for_each = var.subnets
-  resource_group_name  = var.resourcegroup_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  name                 = each.value["name"]
-  address_prefixes     = each.value["address_prefixes"]
-}
 
